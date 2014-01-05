ac2cf9167a4fee0316aa7584a83d5b19d7afe87c
armansansd <a.mohtadji@gmail.com>
2014-01-05 16:04:12 +0100

    files

--- 07_test_cercle/test_cercle/build-tmp/source/test_cercle.java
+++ 07_test_cercle/test_cercle/build-tmp/source/test_cercle.java
@@ -1,7 +1,23 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class test_cercle extends PApplet {
+
 int d ; 
 float posX1, posX2, posY1, posY2, vX1, vY1, vX2, vY2;
 
-void setup() {
+public void setup() {
   size(800, 600);
   noFill();
   smooth();
@@ -16,7 +32,7 @@ void setup() {
 
 
 
-void draw() {
+public void draw() {
   fill(0, 50);
   rect(0, 0, width, height);
   pushMatrix();
@@ -79,3 +95,12 @@ void draw() {
   }
 }
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "test_cercle" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
